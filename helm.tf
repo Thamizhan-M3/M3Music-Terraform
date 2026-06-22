d4f7b329d52d36076dd5780d9434abb0f0265f63
ThamizhanM3 <mitheleshv@gmail.com>
2026-06-22 15:44:40 +0530

    terraform helm v2

--- helm.tf
+++ helm.tf
@@ -18,8 +18,8 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes = {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
 
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
